6bb5674688708dc5d672271bd478331536091464
MazzariniTommaso <93209708+MazzariniTommaso@users.noreply.github.com>
2022-01-15 19:21:52 +0100

    Primo commit

---
+++ src/main/java/it/univpm/ProjectOOP/ProjectOopApplication.java
@@ -0,0 +1,24 @@
+package it.univpm.ProjectOOP;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Avvia l'applicazione Spring
+ * @author Mazzarini Tommaso
+ * @author Merelli Leonardo
+ */
+@SpringBootApplication
+public class ProjectOopApplication 
+{
+	/**
+	 * Main del programma
+	 * @param args argomento del main
+	 */
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(ProjectOopApplication.class, args);
+	} 
+
+}
